daf40a14506ecb02f1655007d2989c711644225c
Jacob <jacobtaric@yahoo.com>
2018-10-18 15:24:06 -0500

    first problem

---
+++ Chapter 1/chapter1_1.java
@@ -0,0 +1,12 @@
+public class chapter1_1 {
+  public static void main(String [] args){
+    String input = "hello";
+    
+    
+    System.out.println("unique");
+    System.out.println("non-unique");
+  }
+  
+  
+  
+}
